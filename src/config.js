0e5b642505da721308af475bb53c0c60e0ab7ef5
luatnd <luatnd@mobiletech.vn>
2019-04-27 20:08:33 +0700

    Config and start to submit form to crawl month data

---
+++ src/config.js
@@ -0,0 +1,4 @@
+exports.default = {
+  dateFrom: '05-01-2002',
+  dateTo: '30-03-2002',
+};
--- src/ketqua.net/soKetQua.js
+++ src/ketqua.net/soKetQua.js
@@ -1,10 +1,11 @@
 // 3rd party lib
 const devices = require('puppeteer/DeviceDescriptors');
+const moment = require('moment');
 
 // app lib
 const { getShotOpt } = require('../utils/screenshot');
 const FileUtil = require('../utils/File');
-
+const Config = require('../config');
 
 const device = devices['Blackberry PlayBook landscape'];
 
@@ -22,9 +23,17 @@ exports.startCrawlAllDailyResult = async function (browser) {
   // Request intercept handler... will be triggered with
   // each page.goto() statement
   await page.goto('http://ketqua.net/so-ket-qua');
-  
-  const datefrom = 2;
-  const dateto = 2;
+
+  //
+
+  let dateFrom = Config.dateFrom;
+  let dateTo = Config.dateTo;
+
+  // If you not configure dateTo, try to set it as now()
+  if (!dateTo) {
+    dateTo = moment().format('DD-MM-YYYY');
+  }
+
   await crawlMonthlyData(page, dateFrom, dateTo);
 }
 
@@ -37,7 +46,10 @@ exports.startCrawlAllDailyResult = async function (browser) {
  */
 async function crawlMonthlyData(page, dateFrom, dateTo) {
   // Type our query into the search bar
-  const dayCount = datediff(dateTo, dateFrom);
+  const df = moment(dateFrom, "DD-MM-YYYY");
+  const dt = moment(dateTo, "DD-MM-YYYY");
+  const dayCount = dt.diff(df, 'd');
+
   const debugKey = dateFrom + '_' + dateTo;
   await page.type('form[action="/so-ket-qua"] input#date', dateFrom);
   await page.type('form[action="/so-ket-qua"] input#count', dayCount);
@@ -67,8 +79,32 @@ async function crawlMonthlyData(page, dateFrom, dateTo) {
  * @param {ElementHandle | String} kqEle
  * TODO: Support pass as string
  */
-exports.parseMonthlyData = function (kqEle) {
+exports.parseMonthlyData = async function (kqEle) {
   // each day: parse daily data
+  // Wait for the results page to load and display the results.
+  const resultsSelector = '.gsc-results .gsc-thumbnail-inside a.gs-title';
+  // await page.waitForSelector(resultsSelector);
+  //
+  // // Extract the results from the page.
+  // const links = await page.evaluate(resultsSelector => {
+  //   const anchors = Array.from(document.querySelectorAll(resultsSelector));
+  //   return anchors.map(anchor => {
+  //     const title = anchor.textContent.split('|')[0].trim();
+  //     return `${title} - ${anchor.href}`;
+  //   });
+  // }, resultsSelector);
+  // console.log(links.join('\n'));
+
+  const dailyDataEle = null;
+  parseDailyData(dailyDataEle);
+}
+
+/**
+ *
+ * @param {momentObj|Date} date
+ * @param {ElementHandle} dailyDataEle
+ */
+function parseDailyData(date, dailyDataEle) {
   const dayData = {
     // Giải: kq cua giai
     0: ["63414"],
@@ -80,7 +116,7 @@ exports.parseMonthlyData = function (kqEle) {
     6: ["42925", "89790"],
     7: ["42925", "89790"],
   }
-  
+
   // save to json file
   FileUtil.saveJson('20020502', dayData);
 }
